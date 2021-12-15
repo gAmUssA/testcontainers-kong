caf02d7f08ca96706cf3f10bbd5c8416c184a296
Viktor Gamov <gamussa@gmail.com>
2021-12-14 21:18:19 -0500

    cleanup

--- src/test/java/com/konghq/testcontainers/KongContainerTest.java
+++ src/test/java/com/konghq/testcontainers/KongContainerTest.java
@@ -1,23 +1,23 @@
 package com.konghq.testcontainers;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.List;
 
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import lombok.extern.slf4j.Slf4j;
 
 import static com.konghq.testcontainers.KongContainer.DEFAULT_PROXY_PORT;
-import static io.restassured.RestAssured.DEFAULT_PORT;
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
 import static org.hamcrest.Matchers.equalTo;
+import static org.testcontainers.containers.BindMode.READ_ONLY;
+import static org.testcontainers.utility.DockerImageName.*;
 
 @Slf4j
 public class KongContainerTest {
@@ -25,21 +25,21 @@ public class KongContainerTest {
   @Test
   void smokeTest() {
     try (
-        final var kongContainer = new KongContainer(DockerImageName.parse("kong:2.6.0-alpine"));
+        final var kongContainer = new KongContainer(parse("kong:2.6.0-alpine"));
 
     ) {
       //kongContainer.withLogConsumer(new Slf4jLogConsumer(log));
-      kongContainer.withClasspathResourceMapping("kong.yaml", "/opt/kong/kong.yaml", BindMode.READ_ONLY);
+      kongContainer.withClasspathResourceMapping("kong.yaml", "/opt/kong/kong.yaml", READ_ONLY);
       kongContainer.withEnv("KONG_DECLARATIVE_CONFIG", "/opt/kong/kong.yaml");
       kongContainer.start();
     }
   }
 
   @Test
-  void smokeTestAdmin() {
-    try (final var kongContainer = new KongContainer(DockerImageName.parse("kong:2.6.0-alpine"))) {
+  void admin_url_should_be_exposed() {
+    try (final var kongContainer = new KongContainer(parse("kong:2.6.0-alpine"))) {
       //kongContainer.withLogConsumer(new Slf4jLogConsumer(log));
-      kongContainer.withClasspathResourceMapping("kong.yaml", "/opt/kong/kong.yaml", BindMode.READ_ONLY);
+      kongContainer.withClasspathResourceMapping("kong.yaml", "/opt/kong/kong.yaml", READ_ONLY);
       kongContainer.withEnv("KONG_DECLARATIVE_CONFIG", "/opt/kong/kong.yaml");
       kongContainer.start();
       get(kongContainer.getaAdminUrl())
@@ -62,31 +62,31 @@ public class KongContainerTest {
   }
 
   @Test
-  void testQuoteService() {
-    try (final var kongContainer = new KongContainer(DockerImageName.parse("kong:2.6.0-alpine"))) {
-      //kongContainer.withLogConsumer(new Slf4jLogConsumer(log));
+  void test_declarative_config_for_QuoteService() {
+    try (final var kong = new KongContainer(parse("kong:2.6.0-alpine"))) {
+      //kong.withLogConsumer(new Slf4jLogConsumer(log));
 
-      kongContainer
+      kong
           .withNetwork(Network.newNetwork())
-          .withClasspathResourceMapping("kong-quotes.yaml", "/opt/kong/quotes.yaml", BindMode.READ_ONLY)
+          .withClasspathResourceMapping("kong-quotes.yaml",
+                                        "/opt/kong/quotes.yaml",
+                                        READ_ONLY)
           .withEnv("KONG_DECLARATIVE_CONFIG", "/opt/kong/quotes.yaml")
-          .withLogConsumer(new Slf4jLogConsumer(log))
+          //.withLogConsumer(new Slf4jLogConsumer(log))
           .withExposedPorts(DEFAULT_PROXY_PORT);
 
-      var quoteService = new GenericContainer<>(DockerImageName.parse("gamussa/native-quote-service:0.0.4-SNAPSHOT"));
-      quoteService
+      var quotes = new GenericContainer<>(parse("gamussa/native-quote-service:0.0.4-SNAPSHOT"));
+      quotes
           .withNetworkAliases("quotes", "quote-service")
-          .withNetwork(kongContainer.getNetwork())
+          .withNetwork(kong.getNetwork())
           // .withLogConsumer(new Slf4jLogConsumer(log))
           .withExposedPorts(8080);
 
-      kongContainer.start();
-      quoteService.start();
+      Startables.deepStart(List.of(kong, quotes)).join();
 
-      get(String.format("http://%s:%s", quoteService.getHost(), quoteService.getMappedPort(8080)))
-          .peek();
+      //get(String.format("http://%s:%s", quotes.getHost(), quotes.getMappedPort(8080))).peek();
 
-      get(String.format("http://%s:%s/", kongContainer.getHost(), kongContainer.getMappedPort(DEFAULT_PROXY_PORT)))
+      get(String.format("http://%s:%s/", kong.getHost(), kong.getMappedPort(DEFAULT_PROXY_PORT)))
           .then()
           .header("Via", containsStringIgnoringCase("kong"));
     }
